e860878f37e12fe9791a394e0a2aefe414a2bf59
KT <thun.kristian@gmail.com>
2023-09-25 10:36:42 +0200

    Created shape, rectangle class, Starting triangle

---
+++ src/Rectangle.java
@@ -0,0 +1,12 @@
+public class Rectangle extends Shape {
+
+    public Rectangle(double height, double width){
+        super(height, width);
+    }
+
+    @Override
+    public double getArea(){
+        return super.getArea();
+    }
+
+}
